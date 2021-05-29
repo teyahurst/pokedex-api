ff51fe2e048f48bd28048c6d258d1603915ee293
Teya Hurst <teyahurst@gmail.com>
2021-05-29 10:42:41 -0500

    added security, and middleware functions

--- server.js
+++ server.js
@@ -1,16 +1,60 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
+const cors = require('cors');
+const helmet = require('helmet');
+
+console.log(process.env.API_TOKEN)
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
-app.use((req, res) => {
-    res.send('Hello, world!')
+
+app.use(function validateBearerToken(req, res, next){
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(401).json({ error: 'Unauthorized Request' })
+    }
+
+    next()
 })
+
+function handleGetTypes(req, res){
+    res.json(validTypes)
+}
+
+function handleGetPokemon(req, res){
+    let response = POKEDEX.pokemon;
+
+    if(req.query.name){
+        response = response.filter(pokemon => 
+            pokemon.name.toLowerCase().includes(req.query.name)
+        )
+    }
+
+    if(req.query.type){
+        response = response.filter(pokemon => 
+            pokemon.type.includes(req.query.type))
+    }
+
+    res.json(response)
+}
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.get('/types', handleGetTypes)
+
+app.get('/pokemon', handleGetPokemon)
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
+
